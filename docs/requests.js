979527eda1a631df3e16fa841a09e0ee0400ab95
= <zubdoob@gmail.com>
2021-07-22 16:45:04 -0500

    bugfix

--- docs/requests.js
+++ docs/requests.js
@@ -1,4 +1,4 @@
-$('#submit1').on('click', predPizza($('#modelurl1').val()));
+
 
 
 // select the text inside of a text input with the id modelurl1 using jquery
@@ -6,3 +6,5 @@ $('#submit1').on('click', predPizza($('#modelurl1').val()));
 let predPizza = (url) => {
     console.log(url)
 }
+
+$('#submit1').on('click', predPizza($('#modelurl1').val()));
