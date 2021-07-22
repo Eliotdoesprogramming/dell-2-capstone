0d8c1e2120772aa5b140e029a87aa84700cf8d42
= <zubdoob@gmail.com>
2021-07-22 17:14:06 -0500

    http request test

--- docs/requests.js
+++ docs/requests.js
@@ -5,6 +5,19 @@
 
 let predPizza = (url) => {
     console.log(url)
+    //make a request to localhost:5000/pizza with a json object containing the url
+    //the server should return a json object with the name of the pizza
+    $.ajax({
+        url: 'http://localhost:5000/pizza',
+        type: 'POST',
+        data: {
+            img: url
+        },
+        success: function(data) {
+            console.log(data)
+            //update the text input with the name of the pizza
+            $('#model1result').append(`<h1>Result: <span class="${data.is_pizza == 'True'? 'badge badge-success': 'badge badge-danger'}">${data.is_pizza == 'True'? 'pizza': 'not pizza'}</span></h1>`) 
+        }
 }
 
 $('#submit1').click(()=>{
