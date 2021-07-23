ca48d15e55c6f2e74bd3c25720bbb0ad7475f302
eliotdoesprogramming <zubdoob@gmail.com>
2021-07-22 23:47:23 -0500

    api calls

--- docs/requests.js
+++ docs/requests.js
@@ -7,13 +7,16 @@ let predPizza = (url) => {
         $.ajax({
             url: 'http://localhost:5000/pizza',
             type: 'POST',
-            data: {
-                img: url
-            },
+            data: url,
             success: function(data) {
-                console.log(data)
                 //update the text input with the name of the pizza
-                $('#model1result').append(`<h1>Result: <span class="${data.is_pizza == 'True'? 'badge badge-success': 'badge badge-danger'}">${data.is_pizza == 'True'? 'pizza': 'not pizza'}</span></h1>`) 
+                $('#model1result').empty();
+                if(data.is_pizza){
+                    $('#model1result').append(`<h1>Result: <span class="${data.is_pizza == 'True'? 'badge badge-success': 'badge badge-danger'}">${data.is_pizza == 'True'? 'pizza': 'not pizza'}</span></h1>`)
+                }
+                else {
+                    $('#model1result').append(`<h1>Error..</h1>`)
+                }   
             }
     })
 }
@@ -22,3 +25,28 @@ $('#submit1').click(()=>{
     predPizza(url)
 })
 console.log('loaded....')
+
+let predTen = (url) => {
+    console.log(url)
+    //make a request to localhost:5000/pizza with a json object containing the url
+    //the server should return a json object with the name of the pizza
+    $.ajax({
+        url: 'http://localhost:5000/tenfood',
+        type: 'POST',
+        data: url,
+        success: function(data) {
+            //update the text input with the name of the pizza
+            $('#model2result').empty();
+            if(data.is_pizza){
+                $('#mode2result').append(`<h1>Result: <span class="badge badge-success">${data.food}</span></h1>`)
+            }
+            else {
+                $('#model2result').append(`<h1>Error..</h1>`)
+            }   
+        }
+})
+}
+$('#submit2').click(()=>{
+let url = $('#modelurl2').val()
+predTen(url)
+})
